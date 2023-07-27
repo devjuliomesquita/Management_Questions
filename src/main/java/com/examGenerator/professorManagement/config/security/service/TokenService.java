6e2b19a1fc54c0febba9f55ca588724cb3028c15
Júlio César Mesquita Camilo Martins <juliocesarmcamilo@gmail.com>
2023-07-27 10:54:31 -0300

    Criado TokenService para criação

---
+++ src/main/java/com/examGenerator/professorManagement/config/security/service/TokenService.java
@@ -0,0 +1,47 @@
+package com.examGenerator.professorManagement.config.security.service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.examGenerator.professorManagement.core.model.ApplicationUser;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+    @Value("${api.security.token.secret}")
+    private String secret;
+    public String genToken(ApplicationUser applicationUser){
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.create()
+                    .withIssuer("auth-api")
+                    .withSubject(applicationUser.getLogin())
+                    .withExpiresAt(genExpDate())
+                    .sign(algorithm);
+
+        }catch (JWTCreationException exception){
+            throw new RuntimeException("Error while generating token", exception);
+        }
+    }
+    public String validationToken(String token){
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("auth-api")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch(JWTVerificationException exception){
+            return "";
+        }
+    }
+    public Instant genExpDate(){
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-02:00"));
+    }
+}
