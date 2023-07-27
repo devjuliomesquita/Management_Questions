604ed9a8649f99a39d1389ca889d27b2c2f98f2a
Júlio César Mesquita Camilo Martins <juliocesarmcamilo@gmail.com>
2023-07-27 00:13:35 -0300

    Initial Commit

---
+++ src/main/java/com/examGenerator/professorManagement/ProfessorManagementApplication.java
@@ -0,0 +1,13 @@
+package com.examGenerator.professorManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessorManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/examGenerator/professorManagement/ProfessorManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examGenerator.professorManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfessorManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
