b7d6b9143f5b2e8d2bf41b926c8f43b937c6a395
Júlio César Mesquita Camilo Martins <juliocesarmcamilo@gmail.com>
2023-07-27 01:27:49 -0300

    Criado um enum para as role

--- src/main/java/com/examGenerator/professorManagement/core/model/ApplicationUser.java
+++ src/main/java/com/examGenerator/professorManagement/core/model/ApplicationUser.java
@@ -7,17 +7,51 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 @Entity
 @Getter
 @Setter
 @Table(name = "tb_user")
-public class ApplicationUser extends AbstractEntity{
+public class ApplicationUser extends AbstractEntity implements UserDetails {
     @NotEmpty
     @Column(unique = true)
-    private String username;
+    private String login;
     @NotEmpty
     private String password;
     @OneToOne
     private Professor professor;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
